3219c071d9a530fb36b92105b549a04fa68f891d
Djerfy <djerfy@gmail.com>
2022-07-20 11:45:13 +0200

    Statefulset implementation (issue #3)

--- src/zabbix-kubernetes-discovery.py
+++ src/zabbix-kubernetes-discovery.py
@@ -12,7 +12,7 @@ from modules.zabbix.discovery import *
 parser = argparse.ArgumentParser()
 parser.add_argument("--zabbix-endpoint", dest="zabbix_endpoint", action="store", required=True, help="Set Zabbix endpoint (server)")
 parser.add_argument("--kubernetes-name", dest="kubernetes_name", action="store", required=True, help="Set Kubernetes cluster name in Zabbix")
-parser.add_argument("--monitoring-mode", dest="monitoring_mode", action="store", required=True, help="Mode of monitoring", choices=["volume","deployment","daemonset","node"])
+parser.add_argument("--monitoring-mode", dest="monitoring_mode", action="store", required=True, help="Mode of monitoring", choices=["volume","deployment","daemonset","node","statefulset"])
 parser.add_argument("--monitoring-type", dest="monitoring_type", action="store", required=True, help="Type of monitoring", choices=["discovery", "item", "json"])
 parser.add_argument("--object-name", dest="object_name", action="store", required=False, help="Name of object in Kubernetes", default=None)
 parser.add_argument("--no-wait", dest="no_wait", action="store_true", required=False, help="Disable startup wait time", default=False)
@@ -84,3 +84,15 @@ if __name__ == "__main__":
         if args.monitoring_type == "item":
             print("Zabbix item (deployment): {}".format(
                 zabbix.send(zabbixItemDeployment(args.kubernetes_name, getDeployment(args.object_name)))))
+
+    # Statefulset
+    if args.monitoring_mode == "statefulset":
+        if args.monitoring_type == "json":
+            print("JSON output (statefulset): {}".format(
+                getStatefulset(args.object_name)))
+        if args.monitoring_type == "discovery":
+            print("Zabbix discovery (statefulset): {}".format(
+                zabbix.send(zabbixDiscoveryStatefulset(args.kubernetes_name, getStatefulset(args.object_name)))))
+        if args.monitoring_type == "item":
+            print("Zabbix item (statefulset): {}".format(
+                zabbix.send(zabbixItemStatefulset(args.kubernetes_name, getStatefulset(args.object_name)))))
