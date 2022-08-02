1a29b9d3778f2dfd38c6fb050c0b991aec61a82f
Djerfy <djerfy@gmail.com>
2022-08-02 13:37:00 +0200

    Add exclusions for object name and namespaces and improve crontab configuration

--- src/zabbix-kubernetes-discovery.py
+++ src/zabbix-kubernetes-discovery.py
@@ -15,6 +15,8 @@ parser.add_argument("--kubernetes-name", dest="kubernetes_name", action="store",
 parser.add_argument("--monitoring-mode", dest="monitoring_mode", action="store", required=True, help="Mode of monitoring", choices=["volume","deployment","daemonset","node","statefulset","cronjob"])
 parser.add_argument("--monitoring-type", dest="monitoring_type", action="store", required=True, help="Type of monitoring", choices=["discovery", "item", "json"])
 parser.add_argument("--object-name", dest="object_name", action="store", required=False, help="Name of object in Kubernetes", default=None)
+parser.add_argument("--exclude-name", dest="exclude_name", action="store", required=False, help="Exclude object name in Kubernetes", default=None)
+parser.add_argument("--exclude-namespace", dest="exclude_namespace", action="store", required=False, help="Exclude namespace in Kubernetes", default=None)
 parser.add_argument("--no-wait", dest="no_wait", action="store_true", required=False, help="Disable startup wait time", default=False)
 args = parser.parse_args()
 
@@ -41,70 +43,70 @@ if __name__ == "__main__":
     if args.monitoring_mode == "node":
         if args.monitoring_type == "json":
             print("JSON output (node): {}".format(
-                getNode(args.object_name)))
+                getNode(args.object_name, args.exclude_name)))
         if args.monitoring_type == "discovery":
             print("Zabbix discovery (node): {}".format(
-                zabbix.send(zabbixDiscoveryNode(args.kubernetes_name, getNode(args.object_name)))))
+                zabbix.send(zabbixDiscoveryNode(args.kubernetes_name, getNode(args.object_name, args.exclude_name)))))
         if args.monitoring_type == "item":
             print("Zabbix item (node): {}".format(
-                zabbix.send(zabbixItemNode(args.kubernetes_name, getNode(args.object_name)))))
+                zabbix.send(zabbixItemNode(args.kubernetes_name, getNode(args.object_name, args.exclude_name)))))
 
     # Daemonset
     if args.monitoring_mode == "daemonset":
         if args.monitoring_type == "json":
             print("JSON output (daemonset): {}".format(
-                getDaemonset(args.object_name)))
+                getDaemonset(args.object_name, args.exclude_name, args.exclude_namespace)))
         if args.monitoring_type == "discovery":
             print("Zabbix discovery (daemonset): {}".format(
-                zabbix.send(zabbixDiscoveryDaemonset(args.kubernetes_name, getDaemonset(args.object_name)))))
+                zabbix.send(zabbixDiscoveryDaemonset(args.kubernetes_name, getDaemonset(args.object_name, args.exclude_name, args.exclude_namespace)))))
         if args.monitoring_type == "item":
             print("Zabbix item (daemonset): {}".format(
-                zabbix.send(zabbixItemDaemonset(args.kubernetes_name, getDaemonset(args.object_name)))))
+                zabbix.send(zabbixItemDaemonset(args.kubernetes_name, getDaemonset(args.object_name, args.exclude_name, args.exclude_namespace)))))
 
     # Volumes
     if args.monitoring_mode == "volume":
         if args.monitoring_type == "json":
             print("JSON output (volume): {}".format(
-                getVolume(args.object_name)))
+                getVolume(args.object_name, args.exclude_name, args.exclude_namespace)))
         if args.monitoring_type == "discovery":
             print("Zabbix discovery (volume): {}".format(
-                zabbix.send(zabbixDiscoveryVolume(args.kubernetes_name, getVolume(args.object_name)))))
+                zabbix.send(zabbixDiscoveryVolume(args.kubernetes_name, getVolume(args.object_name, args.exclude_name, args.exclude_namespace)))))
         if args.monitoring_type == "item":
             print("Zabbix item (volume): {}".format(
-                zabbix.send(zabbixItemVolume(args.kubernetes_name, getVolume(args.object_name)))))
+                zabbix.send(zabbixItemVolume(args.kubernetes_name, getVolume(args.object_name, args.exclude_name, args.exclude_namespace)))))
     
     # Deployment
     if args.monitoring_mode == "deployment":
         if args.monitoring_type == "json":
             print("JSON output (deployment): {}".format(
-                getDeployment(args.object_name)))
+                getDeployment(args.object_name, args.exclude_name, args.exclude_namespace)))
         if args.monitoring_type == "discovery":
             print("Zabbix discovery (deployment): {}".format(
-                zabbix.send(zabbixDiscoveryDeployment(args.kubernetes_name, getDeployment(args.object_name)))))
+                zabbix.send(zabbixDiscoveryDeployment(args.kubernetes_name, getDeployment(args.object_name, args.exclude_name, args.exclude_namespace)))))
         if args.monitoring_type == "item":
             print("Zabbix item (deployment): {}".format(
-                zabbix.send(zabbixItemDeployment(args.kubernetes_name, getDeployment(args.object_name)))))
+                zabbix.send(zabbixItemDeployment(args.kubernetes_name, getDeployment(args.object_name, args.exclude_name, args.exclude_namespace)))))
 
     # Statefulset
     if args.monitoring_mode == "statefulset":
         if args.monitoring_type == "json":
             print("JSON output (statefulset): {}".format(
-                getStatefulset(args.object_name)))
+                getStatefulset(args.object_name, args.exclude_name, args.exclude_namespace)))
         if args.monitoring_type == "discovery":
             print("Zabbix discovery (statefulset): {}".format(
-                zabbix.send(zabbixDiscoveryStatefulset(args.kubernetes_name, getStatefulset(args.object_name)))))
+                zabbix.send(zabbixDiscoveryStatefulset(args.kubernetes_name, getStatefulset(args.object_name, args.exclude_name, args.exclude_namespace)))))
         if args.monitoring_type == "item":
             print("Zabbix item (statefulset): {}".format(
-                zabbix.send(zabbixItemStatefulset(args.kubernetes_name, getStatefulset(args.object_name)))))
+                zabbix.send(zabbixItemStatefulset(args.kubernetes_name, getStatefulset(args.object_name, args.exclude_name, args.exclude_namespace)))))
 
     # Cronjob
     if args.monitoring_mode == "cronjob":
         if args.monitoring_type == "json":
             print("JSON output (cronjob): {}".format(
-                getCronjob(args.object_name)))
+                getCronjob(args.object_name, args.exclude_name, args.exclude_namespace)))
         if args.monitoring_type == "discovery":
             print("Zabbix discovery (cronjob): {}".format(
-                zabbix.send(zabbixDiscoveryCronjob(args.kubernetes_name, getCronjob(args.object_name)))))
+                zabbix.send(zabbixDiscoveryCronjob(args.kubernetes_name, getCronjob(args.object_name, args.exclude_name, args.exclude_namespace)))))
         if args.monitoring_type == "item":
             print("Zabbix item (cronjob): {}".format(
-                zabbix.send(zabbixItemCronjob(args.kubernetes_name, getCronjob(args.object_name)))))
+                zabbix.send(zabbixItemCronjob(args.kubernetes_name, getCronjob(args.object_name, args.exclude_name, args.exclude_namespace)))))
